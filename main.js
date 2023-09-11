82005dbf3b8aef0f5131dc7b41c6c51aa7d39dc7
mohamed hany <me5127538@gmail.com>
2023-09-11 20:51:20 +0300

    add increse numbers to status section

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+
+// start animate width for skills section
 let skills = document.querySelector(".our-skills");
 let progressSpans = document.querySelectorAll(".progress span");
-
 
 window.onscroll = function () {
     if (window.scrollY >= skills.offsetTop) {
@@ -9,3 +10,39 @@ window.onscroll = function () {
         })
     }
 }
+
+// end animate width for skills section
+
+// start increasing nums for stats section
+
+let stats = document.querySelector(".stats");
+let nums = document.querySelectorAll(".stats .number");
+let started = false;
+
+
+window.onscroll = function () {
+    if (window.scrollY >= stats.offsetTop) {
+        if (!started) {
+            nums.forEach((num) => {
+                startCount(num);
+            });
+            started = true;
+        }
+    }
+}
+
+function startCount(element) {
+    let goal = element.dataset.goal;
+
+    let count = setInterval(() => {
+        element.textContent++;
+
+        if (element.textContent === goal) {
+            clearInterval(count);
+        }
+    }, 2000 / goal);
+}
+
+
+// end increasing nums for stats section
+
